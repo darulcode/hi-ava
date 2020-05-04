5e0aa038c12789513e99df675cfb9082e9568858
darulcode <64802429+darulcode@users.noreply.github.com>
2020-05-05 01:27:12 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,8 @@
 // prettier-ignore
 export default [
     { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "antjedtta_", quotes: "When i see you are beutifull\nAWkwokwokwok" },
+    { author: "standinshd", quotes: "Hey! I lovee youuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "standinshd", quotes: "I love you." },
